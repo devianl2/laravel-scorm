591a429c110f89a8506b24632dd97f7ad20a6434
Khaled Lela <eng.khaled.lela@gmail.com>
2022-02-11 20:34:13 +0200

    add extra scorm error translation key

--- resources/lang/en-US/scorm.php
+++ resources/lang/en-US/scorm.php
@@ -1,6 +1,7 @@
 <?php
 return [
-    // SCORM archive messages**
+    // SCORM archive messages
+    'invalid_scorm_archive_message' => 'Invalid SCORM archive.',
     'invalid_scorm_version_message' => 'Invalid SCORM version.',
     'no_sco_in_scorm_archive_message' => 'No items in SCORM archive.',
     'invalid_scorm_data'  => 'Invalid SCORM data.',
@@ -8,7 +9,7 @@ return [
     'invalid_scorm_manifest_identifier' => 'Invalid SCORM manifest identifier.',
     'scorm_disk_not_define' => 'SCORM disk not define',
 
-    // SCORM Items/Children messages**
+    // SCORM Items/Children messages
     'default_organization_not_found_message' => 'SCORM item default organization not found.',
     'no_organization_found_message' => 'No organization found.',
     'sco_with_no_identifier_message' => 'SCORM item without identifier.',
