f4b0cceffa954055824fb64140a732e52f8f07b3
Devian Leong <devianleong@gmail.com>
2022-03-10 17:37:50 +0800

    Added polymorphic function in scorm model. You can use polymorphic to call the scorm model in your project

--- src/Model/ScormModel.php
+++ src/Model/ScormModel.php
@@ -9,6 +9,14 @@ use Illuminate\Database\Eloquent\Model;
 class ScormModel extends Model
 {
 
+    /**
+     * Get the parent resource model (user or post).
+     */
+    public function resourceable()
+    {
+        return $this->morphTo(__FUNCTION__, 'resource_type', 'resource_id');
+    }
+
     public function getTable()
     {
         return config('scorm.table_names.scorm_table', parent::getTable());
